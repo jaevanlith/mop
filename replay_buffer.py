6c13f1ab96475f67bf81b6fa8db875cd15d153c7
ZENBOOK-VAN-JOC\Jochem van Lith <j.a.e.vanlith@student.tudelft.nl>
2024-03-23 11:40:07 +0100

    Remove num_workers from replay buffer

--- replay_buffer.py
+++ replay_buffer.py
@@ -137,7 +137,7 @@ def make_replay_loader(env, replay_dir_list, max_size, batch_size, num_workers, 
 
 	loader = torch.utils.data.DataLoader(iterable,
 										 batch_size=batch_size,
-										 num_workers=num_workers,
+										#  num_workers=num_workers,
 										 pin_memory=True,
 										 worker_init_fn=_worker_init_fn)
 	return loader
