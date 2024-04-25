3691f730ddc72be8b56a2fdc322cab772bc032e2
ZENBOOK-VAN-JOC\Jochem van Lith <j.a.e.vanlith@student.tudelft.nl>
2024-04-25 15:28:48 +0200

    Create actor net with std_dev output

--- agent/pbrl.py
+++ agent/pbrl.py
@@ -23,6 +23,23 @@ class Actor(nn.Module):
 		a = F.relu(self.l1(state))
 		a = F.relu(self.l2(a))
 		return self.max_action * torch.tanh(self.l3(a))
+	
+
+class ActorVar(nn.Module):
+	def __init__(self, state_dim, action_dim, max_action=1):
+		super(Actor, self).__init__()
+
+		self.l1 = nn.Linear(state_dim, 400)
+		self.l2 = nn.Linear(400, 400)
+		# Return mean and std_dev
+		self.l3 = nn.Linear(400, 2*action_dim)
+
+		self.max_action = max_action
+
+	def forward(self, state):
+		a = F.relu(self.l1(state))
+		a = F.relu(self.l2(a))
+		return self.max_action * torch.tanh(self.l3(a))
 
 
 class Critic(nn.Module):
