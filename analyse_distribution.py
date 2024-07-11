47dee062067d4b64f2418457e35cc436e88c9acd
ZENBOOK-VAN-JOC\Jochem van Lith <j.a.e.vanlith@student.tudelft.nl>
2024-07-11 16:48:57 +0200

    Use torch histigrams for distribution shift analysis

--- analyse_distribution.py
+++ analyse_distribution.py
@@ -19,19 +19,15 @@ def kl_divergence(cfg, offline_replay_iter, online_replay_iter, epsilon=1e-10):
     assert offline_states.shape == online_states.shape
 
     print('Retrieveing min and max state values...')
-    min_val = min(offline_states.min(), online_states.min())
-    max_val = max(offline_states.max(), online_states.max())
-    
-    print('Discretizing state space...')
-    bin_edges = np.linspace(min_val, max_val, cfg.bins + 1)
+    min_val = torch.minimum(offline_states.min(dim=0)[0], online_states.min(dim=0)[0])
+    max_val = torch.maximum(offline_states.max(dim=0)[0], online_states.max(dim=0)[0])
+    min_max_vals = tuple(x.item() for x in sum(zip(min_val, max_val), ()))
 
-    print('Estimating offline state distribution...')
-    offline_hist, _ = np.histogram(offline_states, bins=bin_edges, density=True)
-    print('Estimating online state distribution...')
-    online_hist, _ = np.histogram(online_states, bins=bin_edges, density=True)
-    
-    p = torch.tensor(offline_hist + epsilon, dtype=torch.float32)
-    q = torch.tensor(online_hist + epsilon, dtype=torch.float32)
+    p_hist = torch.histogramdd(offline_states, bins=cfg.bins, range=min_max_vals)[0]
+    q_hist = torch.histogramdd(online_states, bins=cfg.bins, range=min_max_vals)[0]
+
+    p = p_hist + epsilon
+    q = q_hist + epsilon
     
     p = p / p.sum()
     q = q / q.sum()
@@ -44,18 +40,18 @@ def kl_divergence(cfg, offline_replay_iter, online_replay_iter, epsilon=1e-10):
 
     return kl_div
 
-def init_replay_buffer(cfg, work_dir, env, offline):    
+def init_replay_buffer(cfg, task, data_type, work_dir, env, offline):    
     # Compose source directory
     replay_buffer_dir = cfg.replay_buffer_dir_offline if offline else cfg.replay_buffer_dir_online
     datasets_dir = work_dir / replay_buffer_dir
     sub_str = "-td3-" if offline else "-"
-    replay_dir = datasets_dir.resolve() / Path(cfg.task + sub_str + str(cfg.data_type)) / 'data'
+    replay_dir = datasets_dir.resolve() / Path(task + sub_str + str(data_type)) / 'data'
     print(f'replay dir: {replay_dir}')
 
     # Construct the replay buffer
     replay_loader = make_replay_loader(env, [replay_dir], cfg.replay_buffer_size,
                                        cfg.batch_size, cfg.replay_buffer_num_workers, cfg.discount,
-                                       main_task=cfg.task, task_list=[cfg.task])
+                                       main_task=task, task_list=[task])
     replay_iter = iter(replay_loader)
 
     return replay_iter
@@ -67,14 +63,25 @@ def main(cfg):
     print(f'workspace: {work_dir}')
 
     # Load data
-    env = dmc.make(cfg.task, seed=cfg.seed)
-    offline_replay_iter = init_replay_buffer(cfg, work_dir, env, offline=True)
-    online_replay_iter = init_replay_buffer(cfg, work_dir, env, offline=False)
+    if cfg.mode == "BxT":
+        env = dmc.make(cfg.task[0], seed=cfg.seed)
+        replay_iter1 = init_replay_buffer(cfg, cfg.task[0], cfg.data_type[0], work_dir, env, offline=True)
+        replay_iter2 = init_replay_buffer(cfg, cfg.task[0], cfg.data_type[0], work_dir, env, offline=False)
+    elif cfg.mode == "TxT":
+        env1 = dmc.make(cfg.task[0], seed=cfg.seed)
+        env2 = dmc.make(cfg.task[1], seed=cfg.seed)
+        replay_iter1 = init_replay_buffer(cfg, cfg.task[0], cfg.data_type[0], work_dir, env1, offline=False)
+        replay_iter2 = init_replay_buffer(cfg, cfg.task[1], cfg.data_type[1], work_dir, env2, offline=False)
+    else:
+        raise ValueError(f'Invalid mode: {cfg.mode}')
     print('Data loaded')
 
     # Measure distribution shift
-    kl_div = kl_divergence(cfg, offline_replay_iter, online_replay_iter)
-    result = f'KL divergence between behavior and teacher policy for {cfg.task}-{cfg.data_type}: {kl_div}'
+    kl_div = kl_divergence(cfg, replay_iter1, replay_iter2)
+    if cfg.mode == "BxT":
+        result = f'KL divergence between behavior and teacher policy for {cfg.task[0]}-{cfg.data_type[0]}: {kl_div}'
+    elif cfg.mode == "TxT":
+        result = f'KL divergence between teacher policy {cfg.task[0]}-{cfg.data_type[0]} and {cfg.task[1]}-{cfg.data_type[1]}: {kl_div}'
     print(result)
 
     f = open("./result.txt", "w")
